15c41c87a1855813bea6b6f7e4ac81d66d0d2410
Nipunn Koorapati <nipunn1313@gmail.com>
2016-08-19 22:13:39 -0700

    Implement insert_slice for SmallVec
    Add benchmark for insert_many

--- benches/bench.rs
+++ benches/bench.rs
@@ -39,6 +39,22 @@ fn bench_insert(b: &mut Bencher) {
 }
 
 #[bench]
+fn bench_insert_many(b: &mut Bencher) {
+    #[inline(never)]
+    fn insert_many_noinline<I: IntoIterator<Item=u64>>(
+        vec: &mut SmallVec<[u64; 16]>, index: usize, iterable: I) {
+        vec.insert_many(index, iterable)
+    }
+
+    b.iter(|| {
+        let mut vec: SmallVec<[u64; 16]> = SmallVec::new();
+        insert_many_noinline(&mut vec, 0, 0..100);
+        insert_many_noinline(&mut vec, 0, 0..100);
+        vec
+    });
+}
+
+#[bench]
 fn bench_extend(b: &mut Bencher) {
     b.iter(|| {
         let mut vec: SmallVec<[u64; 16]> = SmallVec::new();
