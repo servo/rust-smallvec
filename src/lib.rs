383a91e90f7dd0f914e47c58da5f2a328cb64ecc
bolshoytoster <91278344+bolshoytoster@users.noreply.github.com>
2026-08-26 18:55:42 +0000

    Deprecate smallvec! and smallvec_inline! (#445)
    * Deprecate smallvec! and smallvec_inline!
    
    * Disallow single comma as macro input

--- src/lib.rs
+++ src/lib.rs
@@ -2212,6 +2212,7 @@ impl<T, const N: usize> core::iter::FromIterator<T> for SmallVec<T, N> {
     }
 }
 
+#[deprecated]
 #[macro_export]
 macro_rules! smallvec {
     ($elem:expr; $n:expr) => ({
@@ -2222,16 +2223,14 @@ macro_rules! smallvec {
     });
 }
 
+#[deprecated]
 #[macro_export]
 macro_rules! smallvec_inline {
-    // count helper: transform any expression into 1
-    (@one $x:expr) => (1usize);
     ($elem:expr; $n:expr) => ({
         $crate::SmallVec::<_, $n>::from_buf([$elem; $n])
     });
-    ($($x:expr),+ $(,)?) => ({
-        const N: usize = 0usize $(+ $crate::smallvec_inline!(@one $x))*;
-        $crate::SmallVec::<_, N>::from_buf([$($x),*])
+    ($($($x:expr),+$(,)?)?) => ({
+        $crate::SmallVec::from_buf([$($($x),+)?])
     });
 }
 
