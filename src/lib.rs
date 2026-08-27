7c8b485117ca579577bc334d8d5457a98dfbe4ba
Khashayar Fereidani <info@fereidani.com>
2026-08-27 05:28:48 +0330

    Optimize `pop` implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1277,18 +1277,17 @@ impl<T, const N: usize> SmallVec<T, N> {
 
     #[inline]
     pub fn pop(&mut self) -> Option<T> {
-        if self.is_empty() {
-            None
-        } else {
-            let len = self.len() - 1;
-            // SAFETY: len < old_len since this can't overflow, because the old length is
-            // non zero
-            unsafe { self.set_len(len) };
-            // SAFETY: this element was initialized and we just gave up ownership of it, so
-            // we can give it away
-            let value = unsafe { self.as_mut_ptr().add(len).read() };
-            Some(value)
+        let len = self.len();
+        if len == 0 {
+            return None;
         }
+        let new_len = len - 1;
+        // SAFETY: new_len < len since len is non-zero
+        unsafe { self.set_len(new_len) };
+        // SAFETY: this element was initialized and we just gave up ownership of it, so
+        // we can give it away
+        let value = unsafe { self.as_mut_ptr().add(new_len).read() };
+        Some(value)
     }
 
     #[inline]
