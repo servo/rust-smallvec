9ca7b9f61f4ec7900872e812ac59d8ec59ad930a
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-29 02:12:49 +0200

    fix: ran formatter

--- src/defmt.rs
+++ src/defmt.rs
@@ -1,13 +1,15 @@
+use super::SmallVec;
+use defmt::write;
 use defmt::Format;
 use defmt::Formatter;
-use defmt::write;
-use super::SmallVec;
 
 impl<T: Format, const N: usize> Format for SmallVec<T, N> {
     fn format(&self, fmt: Formatter) {
         write!(fmt, "[");
         for (index, element) in self.iter().enumerate() {
-            if index != 0 {write!(fmt, ", ")}
+            if index != 0 {
+                write!(fmt, ", ")
+            }
             write!(fmt, "{:?}", element);
         }
         write!(fmt, "]");
