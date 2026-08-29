17698a1676669b7df951115baae22f2b61c7b7d8
Tobias Decking <Tobias.Decking@gmail.com>
2026-08-29 21:19:46 +0200

    Turn all tests into integration tests

---
+++ tests/serde.rs
@@ -0,0 +1,25 @@
+use smallvec::SmallVec;
+
+#[test]
+fn test_serde() {
+    use serde_test::{assert_tokens, Token};
+    let mut small_vec: SmallVec<i32, 2> = SmallVec::new();
+    assert_tokens(&small_vec, &[Token::Seq { len: Some(0) }, Token::SeqEnd]);
+    small_vec.push(1);
+    assert_tokens(
+        &small_vec,
+        &[Token::Seq { len: Some(1) }, Token::I32(1), Token::SeqEnd],
+    );
+    small_vec.extend([2, 3, 4]);
+    assert_tokens(
+        &small_vec,
+        &[
+            Token::Seq { len: Some(4) },
+            Token::I32(1),
+            Token::I32(2),
+            Token::I32(3),
+            Token::I32(4),
+            Token::SeqEnd,
+        ],
+    );
+}
---
+++ tests/std.rs
@@ -0,0 +1,17 @@
+use smallvec::SmallVec;
+
+#[test]
+fn test_write() {
+    use std::io::Write;
+
+    let data = [1, 2, 3, 4, 5];
+
+    let mut small_vec: SmallVec<u8, 2> = SmallVec::new();
+    let len = small_vec.write(&data[..]).unwrap();
+    assert_eq!(len, 5);
+    assert_eq!(small_vec.as_ref(), data.as_ref());
+
+    let mut small_vec: SmallVec<u8, 2> = SmallVec::new();
+    small_vec.write_all(&data[..]).unwrap();
+    assert_eq!(small_vec.as_ref(), data.as_ref());
+}
