b9f223c5f16e4ea37ec6baf265c7e94f293250aa
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-30 12:15:06 +0000

    fix: added since and note for deprecations (#488)

--- src/lib.rs
+++ src/lib.rs
@@ -2807,7 +2807,7 @@ impl<T, const N: usize> core::iter::FromIterator<T> for SmallVec<T, N> {
     }
 }
 
-#[deprecated]
+#[deprecated(since = "2.0.0-alpha.13", note = "use `SmallVec::from` instead")]
 #[macro_export]
 macro_rules! smallvec {
     ($elem:expr; $n:expr) => ({
@@ -2818,7 +2818,7 @@ macro_rules! smallvec {
     });
 }
 
-#[deprecated]
+#[deprecated(since = "2.0.0-alpha.13", note = "use `SmallVec::from_buf` instead")]
 #[macro_export]
 macro_rules! smallvec_inline {
     ($elem:expr; $n:expr) => ({
