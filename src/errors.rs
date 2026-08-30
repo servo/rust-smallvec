88f1134cd8e200bf9e85c0f9c757a4ae4c694fbe
Yunare <yunare@gmail.com>
2026-08-30 19:05:38 +0000

    refactor: modularize CollectionAllocErr into errors.rs
    Move CollectionAllocErr enum and its Display/Error impls from lib.rs
    to a dedicated src/errors.rs module, re-exporting it from lib.rs to
    preserve the public API.
    
    Fixes #513

---
+++ src/errors.rs
@@ -0,0 +1,27 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use alloc::alloc::Layout;
+
+/// Error type for APIs with fallible heap allocation
+#[derive(Debug)]
+pub enum CollectionAllocErr {
+    /// Overflow `usize::MAX` or other error during size computation
+    CapacityOverflow,
+    /// The allocator return an error
+    AllocErr {
+        /// The layout that was passed to the allocator
+        layout: Layout,
+    },
+}
+
+impl core::fmt::Display for CollectionAllocErr {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "Allocation error: {:?}", self)
+    }
+}
+
+impl core::error::Error for CollectionAllocErr {}
--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,10 @@ extern crate std;
 
 #[cfg(feature = "borsh")]
 mod borsh;
+mod errors;
 mod rawsmallvec;
+
+pub use errors::CollectionAllocErr;
 
 #[cfg(feature = "bytes")]
 use bytes::{
@@ -137,25 +140,6 @@ use {
         }
     }
 };
-
-/// Error type for APIs with fallible heap allocation
-#[derive(Debug)]
-pub enum CollectionAllocErr {
-    /// Overflow `usize::MAX` or other error during size computation
-    CapacityOverflow,
-    /// The allocator return an error
-    AllocErr {
-        /// The layout that was passed to the allocator
-        layout: Layout
-    }
-}
-impl core::fmt::Display for CollectionAllocErr {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "Allocation error: {:?}", self)
-    }
-}
-
-impl core::error::Error for CollectionAllocErr {}
 
 #[inline]
 fn infallible<T>(result: Result<T, CollectionAllocErr>) -> T {
