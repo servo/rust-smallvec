6d45d0164f8d33151d16f58df75befcca296449f
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-31 17:12:26 +0200

    feat: added borsh deserialize and tests

--- src/borsh.rs
+++ src/borsh.rs
@@ -5,9 +5,12 @@ use {
         format
     },
     borsh::{
+        BorshDeserialize,
         BorshSchema,
         BorshSerialize,
         io::{
+            Error,
+            ErrorKind,
             Result as Serial,
             Write
         },
@@ -15,16 +18,29 @@ use {
             Declaration,
             Definition
         }
-    }
+    },
+    core::iter::repeat_with
 };
 
 impl<Type: BorshSerialize, const INLINE: usize> BorshSerialize for SmallVec<Type, INLINE> {
     fn serialize<Writer: Write>(&self, writer: &mut Writer) -> Serial<()> {
-        self.len.value().serialize(writer)?;
+        (self.len.value() as u64).serialize(writer)?;
         for element in self {
             element.serialize(writer)?;
         }
         return Ok(());
+    }
+}
+
+impl<Type: BorshDeserialize, const INLINE: usize> BorshDeserialize for SmallVec<Type, INLINE> {
+    fn deserialize_reader<R: borsh::io::Read>(reader: &mut R) -> Serial<Self> {
+        let length = u64::deserialize_reader(reader)?;
+        return repeat_with(|| Type::deserialize_reader(reader))
+            .take(length.try_into().map_err(|_| Error::new(
+                ErrorKind::OutOfMemory,
+                "Cannot deserialize a sequence with more than usize::MAX elements in this machine"
+            ))?)
+            .collect();
     }
 }
 
@@ -42,8 +58,8 @@ impl<Type: BorshSchema, const INLINE: usize> BorshSchema for SmallVec<Type, INLI
         definitions.insert(
             declaration,
             Definition::Sequence {
-                length_width: usize::BITS as u8 / 8,
-                length_range: 0..=(usize::MAX as u64),
+                length_width: 8,
+                length_range: 0..=u64::MAX,
                 elements: Type::declaration()
             }
         );
