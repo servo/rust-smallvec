8b2016a0aa0113185f381ec0344daabfaa140e0d
Khashayar Fereidani <info@fereidani.com>
2026-09-02 18:04:44 +0000

    style: apply clippy suggestions (#564)

--- src/lib.rs
+++ src/lib.rs
@@ -2583,7 +2583,7 @@ impl<T: Clone, const N: usize> Clone for SmallVec<T, N> {
 
         #[cfg(not(feature = "specialization"))]
         {
-            self.clone_from_fallback(&*source);
+            self.clone_from_fallback(source);
         }
     }
 }
