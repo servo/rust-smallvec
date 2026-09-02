9ebd5ea1f7a56d93c7b8a4c26535d7189d497b92
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-09-02 15:30:40 +0000

    feat: added borsh deserialize and tests (#530)
    * feat: added borsh deserialize and tests
    
    * fix: removed assertion on exact serialization

---
+++ tests/borsh.rs
@@ -0,0 +1,23 @@
+use {
+    borsh::{
+        BorshDeserialize,
+        to_vec
+    },
+    smallvec::SmallVec
+};
+
+#[test]
+fn round_trip() -> () {
+    let smallvec = SmallVec::<u8, 6>::from([1, 2, 3]);
+    let bytes = to_vec(&smallvec).unwrap();
+    let new = SmallVec::<u8, 6>::deserialize(&mut bytes.as_ref()).unwrap();
+    assert_eq!(new, smallvec);
+}
+
+#[test]
+fn round_trip_zst() -> () {
+    let smallvec = SmallVec::<(), 5>::from([(); 0x100000]);
+    let bytes = to_vec(&smallvec).unwrap();
+    let new = SmallVec::<(), 100>::deserialize(&mut bytes.as_ref()).unwrap();
+    assert_eq!(new, smallvec);
+}
