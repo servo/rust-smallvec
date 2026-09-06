58313d305fbb69879cd151c27a3c681a5e09eebe
Khashayar Fereidani <info@fereidani.com>
2026-09-06 12:41:34 +0330

    fix: fix splice reborrow ub

--- src/lib.rs
+++ src/lib.rs
@@ -361,23 +361,17 @@ impl<T, const N: usize> Drain<'_, T, N> {
     /// range. (`replace_with.next()` didn’t return `None`.)
     unsafe fn fill<I: Iterator<Item = T>>(&mut self, replace_with: &mut I) -> bool {
         let vec = unsafe { self.vec.as_mut() };
-        let range_start = vec.len();
         let range_end = self.tail_start;
-        let range_slice = unsafe {
-            core::slice::from_raw_parts_mut(
-                vec.as_mut_ptr().add(range_start),
-                range_end - range_start
-            )
-        };
 
-        for place in range_slice {
-            if let Some(new_item) = replace_with.next() {
-                unsafe {
-                    core::ptr::write(place, new_item);
-                    vec.len.add(1);
-                }
-            } else {
+        while vec.len() < range_end {
+            let Some(new_item) = replace_with.next() else {
                 return false;
+            };
+            let len = vec.len();
+            // SAFETY: len < tail_start <= capacity
+            unsafe {
+                vec.as_mut_ptr().add(len).write(new_item);
+                vec.set_len(len + 1);
             }
         }
         true
@@ -397,9 +391,12 @@ impl<T, const N: usize> Drain<'_, T, N> {
 
         let new_tail_start = self.tail_start + additional;
         unsafe {
-            let src = vec.as_ptr().add(self.tail_start);
-            let dst = vec.as_mut_ptr().add(new_tail_start);
-            core::ptr::copy(src, dst, self.tail_len);
+            let ptr = vec.as_mut_ptr();
+            core::ptr::copy(
+                ptr.add(self.tail_start),
+                ptr.add(new_tail_start),
+                self.tail_len
+            );
         }
         self.tail_start = new_tail_start;
     }
