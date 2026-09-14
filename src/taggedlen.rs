ed4b23aa0191fcec40ccfcffc17170e3c6f11b06
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-09-14 13:45:59 +0000

    fix: overflow on length on not-64-bit (#616)
    * fix: overflow on length on not-64-bit
    
    * fix: style formatting

--- src/taggedlen.rs
+++ src/taggedlen.rs
@@ -57,10 +57,18 @@ impl<T> TaggedLen<T> {
 
     /// # Safety
     ///
-    /// current len+n must be smaller than MAX_LEN
+    /// current len+n must be smaller than MAX_LEN on 64-bit target
     #[inline(always)]
     pub const unsafe fn add(&mut self, n: usize) {
-        debug_assert!(self.len() + n < Self::MAX_LEN);
+        #[cold]
+        #[inline(never)]
+        const fn assert_failed() {
+            panic!("smallvec length overflow")
+        }
+        #[cfg(any(debug_assertions, not(target_pointer_width = "64")))]
+        if self.len().saturating_add(n) > Self::MAX_LEN {
+            assert_failed();
+        }
         self.0 += n << Self::SHIFT;
     }
 
