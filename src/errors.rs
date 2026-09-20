b4b38490904978bf54418e79a6ed686cd07dc781
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-09-20 21:41:55 +0200

    refactor: remove comment

--- src/errors.rs
+++ src/errors.rs
@@ -34,32 +34,3 @@ impl SmallVecError {
         }
     }
 }
-
-// pub trait Handle {
-//    type Handled;
-//    fn handle(self) -> Self::Handled;
-//}
-// impl Handle for SmallVecError {
-//    type Handled = Infallible;
-//
-//    #[cold]
-//    #[inline(never)]
-//    fn handle(self) -> Self::Handled {
-//        match self {
-//            SmallVecError::CapacityOverflow => panic!("smallvec capacity
-// overflow"),            SmallVecError::AllocationError(layout) =>
-// handle_alloc_error(layout)        }
-//    }
-//}
-// impl<Type> Handle for Result<Type, SmallVecError> {
-//    type Handled = Type;
-//
-//    #[inline]
-//    fn handle(self) -> Self::Handled {
-//        match self {
-//            Ok(value) => value,
-//            Err(error) => match error.handle() {}
-//        }
-//    }
-//}
-//
