d86e6d26c091b22e0ea544904771959c31433258
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-09-26 02:27:19 +0200

    Implement `Index` and `IndexMut` traits
    It's useful to have `Index` and `IndexMut` traits implemented if you
    want to have code that is generic over indexable containers, like this:
    
    ```
    fn increment<C>(container: &mut C, index: usize)
    where
        C: std::ops::IndexMut<usize>,
        C::Output: std::ops::AddAssign<i32>,
    {
        container[index] += 1;
    }
    ```
    
    Having these traits is also consistent with `Vec`, which also implements
    them.
    
    I have added two tests to test these traits.

--- src/references.rs
+++ src/references.rs
@@ -10,8 +10,11 @@ use {
         },
         ops::{
             Deref,
-            DerefMut
-        }
+            DerefMut,
+            Index,
+            IndexMut
+        },
+        slice::SliceIndex
     }
 };
 
@@ -53,5 +56,21 @@ impl<T, const N: usize, A: Allocator> DerefMut for SmallVec<T, N, A> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.as_mut_slice()
+    }
+}
+
+impl<T, const N: usize, A: Allocator, I: SliceIndex<[T]>> Index<I> for SmallVec<T, N, A> {
+    type Output = <I as SliceIndex<[T]>>::Output;
+
+    #[inline]
+    fn index(&self, index: I) -> &Self::Output {
+        &self.deref()[index]
+    }
+}
+
+impl<T, const N: usize, A: Allocator, I: SliceIndex<[T]>> IndexMut<I> for SmallVec<T, N, A> {
+    #[inline]
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        &mut self.deref_mut()[index]
     }
 }
